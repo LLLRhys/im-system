4471fbf7d680349230f1ea5ed2aa7b152e5224ab
LLLRhys <2528765108@qq.com>
2025-08-07 17:22:53 +0800

    将server,go中的handle函数进行了封装，封装成了User的方法

--- go-im-system/server.go
+++ go-im-system/server.go
@@ -2,49 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
 
 type Server struct {
-	IP string
+	IP   string
 	Port int
 
 	//在线用户的列表
 	OnlineMap map[string]*User
-	Maplock sync.RWMutex
+	Maplock   sync.RWMutex
 
 	//用于广播的管道
 	Ch chan string
 }
 
-//创建一个server的接口
+// 创建一个server的接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		IP: 		ip,
-		Port: 		port,
-		OnlineMap: 	make(map[string]*User),
-		Ch: 		make(chan string),
+		IP:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Ch:        make(chan string),
 	}
 	return server
 }
 
-//准备上线信息的方法
+// 准备上线信息的方法
 func (s *Server) BroadCast(user *User, mes string) {
-	sendmes := "[" + user.Addr + "]" + user.Name + ":"+ mes
+	sendmes := "[" + user.Addr + "]" + user.Name + ":" + mes
 
 	s.Ch <- sendmes
 }
 
-//监听server的广播消息，有消息就发送上线信息给所有在线的user
+// 监听server的广播消息，有消息就发送上线信息给所有在线的user
 func (s *Server) LisenMessage() {
 	for {
-		mes := <- s.Ch
+		mes := <-s.Ch
 
 		//向用户发送信息
 		s.Maplock.Lock()
-		for _,user := range s.OnlineMap {
-			user.Ch <- mes  //会阻塞 要单独开个gorutine
+		for _, user := range s.OnlineMap {
+			user.Ch <- mes //会阻塞 要单独开个gorutine
 		}
 		s.Maplock.Unlock()
 	}
@@ -54,24 +55,43 @@ func (s *Server) Handler(conn net.Conn) {
 	//...当前业务的链接
 	//fmt.Println("链接建立成功")
 
-	user := NewUser(conn)
+	user := NewUser(conn, s)
 
-	//用户上线，添加到用户表中
-	s.Maplock.Lock()
-	s.OnlineMap[user.Name] = user
-	s.Maplock.Unlock()
+	//用户的上线部分
+	user.Online()
 
-	//准备当前用户上线信息
-	s.BroadCast(user, "你好")
+	//接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf) //阻塞，直到有数据到来
+			if n == 0 {
+				//用户下线
+				user.Offline()
+				return
+			}
+			//如果是EOF，说明只是文件末尾
+			if err != nil && err != io.EOF {
+				fmt.Println("conn.Read error:", err)
+				return
+			}
+
+			//提取用户发送的信息（最后一位是换行‘/n’）
+			mes := string(buf[0 : n-1])
+
+			//用户对mes进行消息处理
+			user.DoMessage(mes)
+		}
+	}() //这个圆括号很重要，前面func(){}只是定义了一个匿名函数，后面的()才是调用这个函数。
 
 	//阻塞当前handle
 	select {}
 }
 
-//启动服务器的接口
+// 启动服务器的接口
 func (s *Server) Start() {
 	//socket listen
-	listener, err := net.Listen("tcp",fmt.Sprintf("%s:%d", s.IP, s.Port))
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.IP, s.Port))
 	if err != nil {
 		fmt.Println("net.Listen error:", err)
 		return
@@ -84,13 +104,13 @@ func (s *Server) Start() {
 
 	for {
 		//accept
-		conn, err := listener.Accept()  //不断阻塞接收，直到有客户端连接。接受后返回客户端对象conn。conn可以理解为一个客户端的连接实例。
+		conn, err := listener.Accept() //不断阻塞接收，直到有客户端连接。接受后返回客户端对象conn。conn可以理解为一个客户端的连接实例。
 		if err != nil {
 			fmt.Println("Listener.Accept error:", err)
 			continue
 		}
 
 		// do handler
-		go s.Handler(conn)  //客户端对象重新开一个线程。
+		go s.Handler(conn) //客户端对象重新开一个线程。
 	}
 }
--- go-im-system/user.go
+++ go-im-system/user.go
@@ -7,16 +7,19 @@ type User struct {
 	Addr string
 	Ch   chan string
 	Conn net.Conn
+
+	Server *Server
 }
 
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	username := conn.RemoteAddr().String()
 
 	user := &User{
-		Name: 	username,
-		Addr: 	username,
-		Ch: 	make(chan string),
-		Conn: 	conn,
+		Name:   username,
+		Addr:   username,
+		Ch:     make(chan string),
+		Conn:   conn,
+		Server: server,
 	}
 
 	go user.ListenMessage()
@@ -24,10 +27,34 @@ func NewUser(conn net.Conn) *User {
 	return user
 }
 
+func (u *User) Online() {
+	//用户上线，添加到用户表中
+	u.Server.Maplock.Lock()
+	u.Server.OnlineMap[u.Name] = u
+	u.Server.Maplock.Unlock()
+
+	//广播当前用户上线信息
+	u.Server.BroadCast(u, "已上线")
+}
+
+func (u *User) Offline() {
+	//用户上线，添加到用户表中
+	u.Server.Maplock.Lock()
+	delete(u.Server.OnlineMap, u.Name)
+	u.Server.Maplock.Unlock()
+
+	//广播当前用户上线信息
+	u.Server.BroadCast(u, "已下线")
+}
+
+func (u *User) DoMessage(mes string) {
+	u.Server.BroadCast(u, mes)
+}
+
 func (u *User) ListenMessage() {
 	for {
-		mesg := <-u.Ch	//一直接收消息，或阻塞
+		mesg := <-u.Ch //一直接收消息，或阻塞
 
-		u.Conn.Write([]byte(mesg+"\n"))  //写到客户端
+		u.Conn.Write([]byte(mesg + "\n")) //写到客户端
 	}
 }
