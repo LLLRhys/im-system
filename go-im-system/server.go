962e63c45d11f5c12a30d76a68c17463d63db1b4
LLLRhys <2528765108@qq.com>
2025-08-17 12:30:52 +0800

    完善了server.go中的handler，增加了超时强制下线的功能

--- go-im-system/server.go
+++ go-im-system/server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -60,6 +61,9 @@ func (s *Server) Handler(conn net.Conn) {
 	//用户的上线部分
 	user.Online()
 
+	//监听用户是否活跃的channel
+	isLive := make(chan bool)
+
 	//接受客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
@@ -81,11 +85,38 @@ func (s *Server) Handler(conn net.Conn) {
 
 			//用户对mes进行消息处理
 			user.DoMessage(mes)
+
+			//用户发送了消息，用户活跃
+			isLive <- true
 		}
 	}() //这个圆括号很重要，前面func(){}只是定义了一个匿名函数，后面的()才是调用这个函数。
 
 	//阻塞当前handle
-	select {}
+	for {
+		select {
+		case <-isLive:
+			//当前用户活跃，重置定时器
+			//不做任何事，就是为了重新触发下面的定时器
+
+		case <-time.After(time.Second * 30):
+			//已经超时  将当前User强制关闭
+			user.SendMes("超时，您被强制下线了\n")
+
+			//销毁用的资源
+			close(user.Ch)
+
+			//关闭连接
+			conn.Close() //客户端下线 还会发送一个”0“，触发User的OffLine
+
+			//conn.Close()之后user还在，只是管道被关闭了，此时还和和还能触发user的OffLine方法
+			//retuen之后user才会消失（handler中的局部变量）
+
+			//退出当前的handler
+			return //runtime.Goexit()
+		}
+
+	}
+
 }
 
 // 启动服务器的接口
